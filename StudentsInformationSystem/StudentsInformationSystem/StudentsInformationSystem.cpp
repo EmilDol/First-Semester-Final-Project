50565ab920db718e88bdb59e6201728780224fd4
Emil Dolchinkov <emildolchinkov@gmail.com>
2024-12-24 00:16:22 +0200

    base project done

--- StudentsInformationSystem/StudentsInformationSystem/StudentsInformationSystem.cpp
+++ StudentsInformationSystem/StudentsInformationSystem/StudentsInformationSystem.cpp
@@ -3,10 +3,14 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
-const string MENU = "1. Input n-count students.\n2. Show all students.\n3. Search in the students list.\n4. Sort students alphabetically by first name.\n5. Options for working with external files.\n0. Exit.\nPlease input command number: ";
+const string MENU = "1. Input n-count students.\n2. Show all students.\n3. Search in the students list.\n4. Sort students alphabetically by first name.\n5. Export students to binary file.\n6. Import students from binary file.\n0. Exit.\nPlease input command number: ";
+
+const string SEARCH_SUBMENU = "1. By programming basics grade [min - max].\n2. By failed courses.\n0. Back.\nPlease input command number: ";
 
 enum Gender
 {	
@@ -24,7 +28,7 @@ enum Status
 struct Subject
 {
 	string Name;
-	int Grade;
+	unsigned short Grade;
 };
 
 struct Student
@@ -39,8 +43,242 @@ struct Student
 	Subject subjects[5];
 };
 
+void showStudents(const Student students[30], const unsigned short& studentsCount);
+unsigned short searchForFailed(const Student students[30], const unsigned short& studentsCount, Student found[30]);
+unsigned short searchByProgrammingBasics(const Student students[30], const unsigned short& studentsCount, int min, int max, Student found[30]);
+
+void importStudents(Student students[30], unsigned short& studentsCount) {
+	ifstream inFile("students.dat", ios::binary);
+	if (!inFile) {
+		cerr << "Error opening file for reading!" << endl;
+		return;
+	}
+
+	studentsCount = 0;
+	while (inFile.read(students[studentsCount].facultyNumber, sizeof(students[studentsCount].facultyNumber))) {
+		inFile.read(students[studentsCount].EGN, sizeof(students[studentsCount].EGN));
+
+		char buffer[256];
+		inFile.getline(buffer, 256, '\0');
+		students[studentsCount].firstName = buffer;
+
+		inFile.getline(buffer, 256, '\0');
+		students[studentsCount].middleName = buffer;
+
+		inFile.getline(buffer, 256, '\0');
+		students[studentsCount].lastName = buffer;
+
+		inFile.read(reinterpret_cast<char*>(&students[studentsCount].gender), sizeof(students[studentsCount].gender));
+		inFile.read(reinterpret_cast<char*>(&students[studentsCount].status), sizeof(students[studentsCount].status));
+
+		for (int j = 0; j < 5; ++j) {
+			inFile.getline(buffer, 256, '\0');
+			students[studentsCount].subjects[j].Name = buffer;
+			inFile.read(reinterpret_cast<char*>(&students[studentsCount].subjects[j].Grade), sizeof(students[studentsCount].subjects[j].Grade));
+		}
+
+		studentsCount++;
+		if (studentsCount >= 30) break; // Prevent overflow
+	}
+
+	inFile.close();
+	cout << "Students imported successfully!" << endl;
+}
+
+void exportStudents(const Student students[30], const unsigned short& studentsCount) 
+{
+	ofstream outFile("students.dat", ios::binary);
+	if (!outFile) {
+		cerr << "Error opening file for writing!" << endl;
+		return;
+	}
+
+	for (int i = 0; i < studentsCount; ++i) {
+		outFile.write(students[i].facultyNumber, sizeof(students[i].facultyNumber));
+		outFile.write(students[i].EGN, sizeof(students[i].EGN));
+		outFile.write(students[i].firstName.c_str(), students[i].firstName.size() + 1);
+		outFile.write(students[i].middleName.c_str(), students[i].middleName.size() + 1);
+		outFile.write(students[i].lastName.c_str(), students[i].lastName.size() + 1);
+		outFile.write(reinterpret_cast<const char*>(&students[i].gender), sizeof(students[i].gender));
+		outFile.write(reinterpret_cast<const char*>(&students[i].status), sizeof(students[i].status));
+		for (int j = 0; j < 5; ++j) {
+			outFile.write(students[i].subjects[j].Name.c_str(), students[i].subjects[j].Name.size() + 1);
+			outFile.write(reinterpret_cast<const char*>(&students[i].subjects[j].Grade), sizeof(students[i].subjects[j].Grade));
+		}
+	}
+
+	outFile.close();
+	cout << "Students exported successfully!" << endl;
+}
+
+void orderByName(Student students[30], const unsigned short& count)
+{
+	for (int i = 0; i < count - 1; i++)
+	{
+		bool swapped = false;
+		for (int j = i + 1; j < count; j++)
+		{
+			if (students[i].firstName > students[j].firstName)
+			{
+				swapped = true;
+				Student temp = students[i];
+				students[i] = students[j];
+				students[j] = temp;
+			}
+		}
+		if (!swapped)
+		{
+			break;
+		}
+	}
+}
+
+void searchSubmenu(const Student students[30], const unsigned short studentsCount)
+{
+	unsigned short cmd, count;
+	Student found[30];
+	do
+	{
+		cout << SEARCH_SUBMENU;
+		cin >> cmd;
+		switch (cmd)
+		{
+			case 1:
+			{
+				unsigned short minGrade, maxGrade;
+				bool minMaxValid = true;
+				do
+				{
+					if (!minMaxValid)
+					{
+						cout << "Please input min and max grade again. This time try making the min lesser than the max" <<		endl;
+					}
+					cout << "Please input the minimum grade: ";
+					cin >> minGrade;
+					while (minGrade < 2 || minGrade > 6)
+					{
+						cout << "Please input valid minimum grade: ";
+						cin >> minGrade;
+					}
+					cout << "Please input the maximum grade: ";
+					cin >> maxGrade;
+					while (maxGrade < 2 || maxGrade > 6)
+					{
+						cout << "Please input valid minimum grade: ";
+						cin >> minGrade;
+					}
+					minMaxValid = minGrade <= maxGrade;
+				} while (minGrade > maxGrade);
+				count = searchByProgrammingBasics(students, studentsCount, minGrade, maxGrade, found);
+
+				if (count == 0)
+				{
+					cout << "No students found with grade between " << minGrade << " and " << maxGrade << " in Programming		Basics!" << endl;
+				}
+				else
+				{
+					showStudents(found, count);
+				}
+				break;
+			}
+			case 2:
+				count = searchForFailed(students, studentsCount, found);
+
+				if (count == 0)
+				{
+					cout << "All students passed their exams!" << endl;
+				}
+				else
+				{
+					showStudents(found, count);
+				}
+				break;
+			default:
+				if (cmd != 0)
+					cout << "Invalid command number!" << endl;
+				break;
+		}
+	} while (cmd != 0);
+}
+
+unsigned short searchForFailed(const Student students[30], const unsigned short &studentsCount, Student found[30])
+{
+	unsigned short count = 0;
+	for (int i = 0; i < studentsCount; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (students[i].subjects[j].Grade == 2)
+			{
+				found[count++] = students[i];
+				break;
+			}
+		}
+	}
+
+	return count;
+}
+
+unsigned short searchByProgrammingBasics(const Student students[30], const unsigned short &studentsCount, int min, int max, Student found[30])
+{
+	unsigned short count = 0;
+	for (int i = 0; i < studentsCount; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (students[i].subjects[j].Name == "PB" && students[i].subjects[j].Grade >= min && students[i].subjects[j].Grade <= max)
+			{
+				found[count++] = students[i];
+				break;
+			}
+		}
+	}
+	return count;
+}
+
+void showStudents(const Student students[30], const unsigned short &studentsCount)
+{
+	cout << left << setw(10) << "Faculty#"
+		<< setw(12) << "EGN"
+		<< setw(15) << "First Name"
+		<< setw(15) << "Middle Name"
+		<< setw(15) << "Last Name"
+		<< setw(8) << "Gender"
+		<< setw(10) << "Status"
+		<< setw(15) << "Subject 1"
+		<< setw(6) << "Grade"
+		<< setw(15) << "Subject 2"
+		<< setw(6) << "Grade"
+		<< setw(15) << "Subject 3"
+		<< setw(6) << "Grade"
+		<< setw(15) << "Subject 4"
+		<< setw(6) << "Grade"
+		<< setw(15) << "Subject 5"
+		<< setw(6) << "Grade"
+		<< endl;
+
+	for (int i = 0; i < studentsCount; i++)
+	{
+		cout << left << setw(10) << students[i].facultyNumber
+			<< setw(12) << students[i].EGN
+			<< setw(15) << students[i].firstName
+			<< setw(15) << students[i].middleName
+			<< setw(15) << students[i].lastName
+			<< setw(8) << (students[i].gender == Male ? "Male" : "Female")
+			<< setw(10) << (students[i].status == Active ? "Active" : (students[i].status == Dropout ? "Dropout" : "Graduated"));
+
+		for (int j = 0; j < 5; j++)
+		{
+			cout << setw(15) << students[i].subjects[j].Name
+				<< setw(6) << students[i].subjects[j].Grade;
+		}
+		cout << endl;
+	}
+}
+
 void inputStudents(Student students[30], unsigned short &studentsCount)
 {
+	unsigned short initCount = studentsCount;
 	unsigned short countToInput;
 	cout << "Please input the number of students that you want to add: ";
 	cin >> countToInput;
@@ -52,21 +290,21 @@ void inputStudents(Student students[30], unsigned short &studentsCount)
 	for (int i = 0; i < countToInput; i++)
 	{
 		cin.ignore(1000, '\n');
-		cout << "Student ¹" << i + 1 << ":\n";
+		cout << "Student #" << initCount + i + 1 << ":\n";
 		cout << "First name: ";
-		getline(cin, students[studentsCount + i].firstName);
+		getline(cin, students[initCount + i].firstName);
 
 		cout << "Middle name: ";
-		getline(cin, students[studentsCount + i].middleName);
+		getline(cin, students[initCount + i].middleName);
 
 		cout << "Last name: ";
-		getline(cin, students[studentsCount + i].lastName);
+		getline(cin, students[initCount + i].lastName);
 
 		cout << "Faculty number: ";
-		cin.getline(students[studentsCount + i].facultyNumber, 9);
+		cin.getline(students[initCount + i].facultyNumber, 9);
 
 		cout << "EGN: ";
-		cin.getline(students[studentsCount + i].EGN, 11);
+		cin.getline(students[initCount + i].EGN, 11);
 
 		cout << "Gender (1 for male and 2 for female): ";
 		unsigned short genderInput;
@@ -76,7 +314,7 @@ void inputStudents(Student students[30], unsigned short &studentsCount)
 			cout << "Please enter a valid gender: ";
 			cin >> genderInput;
 		}
-		students[studentsCount + i].gender = Gender(genderInput);
+		students[initCount + i].gender = Gender(genderInput);
 
 		cout << "Status (1 for active, 2 for dropout and 3 for graduated): ";
 		unsigned short statusInput;
@@ -86,16 +324,21 @@ void inputStudents(Student students[30], unsigned short &studentsCount)
 			cout << "Please enter a valid status: ";
 			cin >> statusInput;
 		}
-		students[studentsCount + i].status = Status(statusInput);
+		students[initCount + i].status = Status(statusInput);
 
 		cout << "Subjects: " << endl;
 		for (int j = 0; j < 5; j++)
 		{
 			cin.ignore(1000, '\n');
 			cout << "	Name: ";
-			getline(cin, students[studentsCount + i].subjects[j].Name);
-			cout << "	Grade for " << students[studentsCount + i].subjects[j].Name << " : ";
-			cin >> students[studentsCount + i].subjects[j].Grade;
+			getline(cin, students[initCount + i].subjects[j].Name);
+			cout << "	Grade for " << students[initCount + i].subjects[j].Name << " : ";
+			cin >> students[initCount + i].subjects[j].Grade;
+			while ((students[initCount + i].subjects[j].Grade < 2 || students[initCount + i].subjects[j].Grade > 6) && students[initCount + i].subjects[j].Grade != 0)
+			{
+				cout << "Please enter a valid grade: ";
+				cin >> students[initCount + i].subjects[j].Grade;
+			}
 		}
 		studentsCount++;
 	}
@@ -114,14 +357,20 @@ int mainMenu(Student students[30], unsigned short &studentsCount)
 			inputStudents(students, studentsCount);
 			break;
 		case 2:
+			showStudents(students, studentsCount);
 			break;
 		case 3:
+			searchSubmenu(students, studentsCount);
 			break;
 		case 4:
+			orderByName(students, studentsCount);
+			showStudents(students, studentsCount);
 			break;
 		case 5:
+			exportStudents(students, studentsCount);
 			break;
 		case 6:
+			importStudents(students, studentsCount);
 			break;
 		default:
 			if (cmd != 0)
@@ -138,14 +387,3 @@ int main()
 	unsigned short studentsCount = 0;
 	mainMenu(students, studentsCount);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
